bf152c08491017a318a0069380e9938fd191d4d3
dgabriell <joel.gabriel.lopez@outlook.com>
2022-05-11 20:02:41 -0600

    Formulario tipo combustible
    Agregué formulario tipo de combustible, todo funcionando correctamente

---
+++ proyectoGasolinera/frmTipoCombustible.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace proyectoGasolinera
+{
+    public partial class frmTipoCombustible : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            wsTipoCombustible.wsTipoCombustibleSoapClient ws = new wsTipoCombustible.wsTipoCombustibleSoapClient();
+            DataSet ds = ws.Listado_Tipo_De_Combustible();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.AutoGenerateSelectButton = true;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtID.Text = GridView1.SelectedRow.Cells[1].Text.ToString();
+            txtNombre.Text = GridView1.SelectedRow.Cells[2].Text.ToString();
+            txtPrecioAuto.Text = GridView1.SelectedRow.Cells[3].Text.ToString();
+            txtPrecioCompleto.Text = GridView1.SelectedRow.Cells[4].Text.ToString();
+        }
+
+        protected void btnGuardar_Click1(object sender, EventArgs e)
+        {
+            wsTipoCombustible.wsTipoCombustibleSoapClient ws = new wsTipoCombustible.wsTipoCombustibleSoapClient();
+            ws.Insertar_Tipo_De_Combustible(txtNombre.Text, double.Parse(txtPrecioAuto.Text), double.Parse(txtPrecioCompleto.Text));
+
+            DataSet ds = ws.Listado_Tipo_De_Combustible();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.AutoGenerateSelectButton = true;
+            GridView1.DataBind();
+            limpiar();
+        }
+
+        protected void btnActualizar_Click(object sender, EventArgs e)
+        {
+            wsTipoCombustible.wsTipoCombustibleSoapClient ws = new wsTipoCombustible.wsTipoCombustibleSoapClient();
+            ws.actualizar_Combustible(int.Parse(txtID.Text), txtNombre.Text, double.Parse(txtPrecioAuto.Text), double.Parse(txtPrecioCompleto.Text));
+
+            DataSet ds = ws.Listado_Tipo_De_Combustible();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.AutoGenerateSelectButton = true;
+            GridView1.DataBind();
+            limpiar();
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            wsTipoCombustible.wsTipoCombustibleSoapClient ws = new wsTipoCombustible.wsTipoCombustibleSoapClient();
+            ws.eliminar_Combustible(int.Parse(txtID.Text));
+
+            DataSet ds = ws.Listado_Tipo_De_Combustible();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.AutoGenerateSelectButton = true;
+            GridView1.DataBind();
+            limpiar();
+        }
+
+        public void limpiar()
+        {
+            txtID.Text = "";
+            txtNombre.Text = "";
+            txtPrecioAuto.Text = "";
+            txtPrecioCompleto.Text = "";
+        }
+
+        protected void txtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ proyectoGasolinera/frmTipoCombustible.aspx.designer.cs
@@ -0,0 +1,152 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace proyectoGasolinera
+{
+
+
+    public partial class frmTipoCombustible
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblID;
+
+        /// <summary>
+        /// Control txtID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtID;
+
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control lblPrecioAuto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPrecioAuto;
+
+        /// <summary>
+        /// Control txtPrecioAuto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPrecioAuto;
+
+        /// <summary>
+        /// Control lblPrecioCompleto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPrecioCompleto;
+
+        /// <summary>
+        /// Control txtPrecioCompleto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPrecioCompleto;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnActualizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnActualizar;
+
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+
+        /// <summary>
+        /// Control lblBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBuscar;
+
+        /// <summary>
+        /// Control txtBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBuscar;
+
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+    }
+}
